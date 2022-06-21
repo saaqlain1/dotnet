7ea3dd751575fd991aca0bb0475996c5e1c68faa
saaqlain1 <42904345+saaqlain1@users.noreply.github.com>
2022-06-21 16:36:43 +1000

    Add files via upload

---
+++ Helloworld.cs
@@ -0,0 +1,17 @@
+using System;
+
+class HelloWorld
+{
+
+	static void Main()
+{
+
+#if DebugConfig
+Console.WriteLine("We are in the Debug Configuration");
+#endif
+
+Console.WriteLine("MSBuild is working fine");
+
+}
+
+}
